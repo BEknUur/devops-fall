d25aec4183fbdb1d86d7f04bc2cefd3dc5711d0a
Beknur <ualihanulybeknur@gmail.com>
2026-09-16 10:43:42 +0500

    first task1

---
+++ practice-2/task1.py
@@ -0,0 +1,21 @@
+def failed_deployments(path:str):
+    """
+    Yield only failed deployments as (service, reason) tuples
+    """
+
+    with open(path) as f:
+        for line in f:
+            parts = line.split()
+
+            if len(parts)!=4:
+                continue
+
+            _date, service, status, reason = parts
+            if status =="FAILED":
+                yield service,reason
+
+
+
+if __name__ =="__main__":
+    for service,reason in failed_deployments("test.log"):
+        print(service,reason)
